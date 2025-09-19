7e411ae7a79bb1ea3e39744a7fbc4469c18979ec
JOÃO_CELLA124312 <joao.cella.luz@escola.pr.gov.br>
2025-09-19 08:55:03 -0300

    Add files via upload

---
+++ trabalhos java/1909-2.js
@@ -0,0 +1,8 @@
+const cities = new Array("nova york", "guaruja", "rio de janeiro", "salvador", "curitiba");
+
+for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+}
+
+cities.push("teste");
+console.log(cities);
---
+++ trabalhos java/1909.js
@@ -0,0 +1,17 @@
+const listacompra=[".Consolo","sonifero","lumbrificante"]  
+listacompra.push("corrente")
+console.log(listacompra)
+
+listacompra.pop()
+console.log(listacompra)
+
+listacompra.push("Russa")
+console.log(listacompra)
+
+listacompra.pop()
+console.log(listacompra)
+
+
+console.log(listacompra.indexOf("Consolo"))
+listacompra[1]="sequestro"
+
