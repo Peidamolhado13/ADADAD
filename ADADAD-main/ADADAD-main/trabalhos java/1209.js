386a4a3390e53881805a580edc2173fadb213682
JOÃO_CELLA124312 <joao.cella.luz@escola.pr.gov.br>
2025-11-04 10:44:30 -0300

    Add files via upload

---
+++ ADADAD-main/ADADAD-main/trabalhos java/1209.js
@@ -0,0 +1,14 @@
+console.log("trabalhando com lestras")
+const NovaYorK = 'Nova York';
+const Cairo='Cairo';
+const Vancuver='Vancuver';
+const Tokio ='Tokiyo';
+const França='França';
+console.log(
+    NovaYorK,
+    Cairo,
+    Vancuver,
+    Tokio,
+    França);
+
+    
