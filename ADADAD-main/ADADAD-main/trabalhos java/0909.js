bd6abcb14528bf6116d59004eec5f4edc0a2b5d2
JOÃO_CELLA124312 <joao.cella.luz@escola.pr.gov.br>
2025-11-14 08:04:21 -0300

    Delete ADADAD-main/ADADAD-main/trabalhos java directory

--- ADADAD-main/ADADAD-main/trabalhos java/0909.js
+++
@@ -1,5 +0,0 @@
- console.log("trabalho com atribuição de variável");//inerposição 
-const idade= 16
-const nome= "naruto"
-console.sobrenome ="uzumaki"
-console.log(nome+" "+uzumaki)
--- ADADAD-main/ADADAD-main/trabalhos java/1909-2.js
+++
@@ -1,8 +0,0 @@
-const cities = new Array("nova york", "guaruja", "rio de janeiro", "salvador", "curitiba");
-
-for (let i = 0; i < cities.length; i++) {
-    console.log(cities[i]);
-}
-
-cities.push("teste");
-console.log(cities);
